a7d3206076bc3f2a5797cd0c5b64196182aca306
rhaelfixer <rhaelfixer@gmail.com>
2023-12-25 08:41:47 +0900

    Add Education

---
+++ src/views/Education.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {Card} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+// CSS
+import "./styles/Education.css";
+
+
+function Education() {
+  return (
+    <section className="education-CSS" id="education">
+      <Card className="education-outer-card-CSS">
+        <h1 className="education-h1-CSS">
+          Education
+        </h1>
+        <Card className="education-inner-card-CSS">
+          <Card.Body>
+            <Card.Title>
+              <h2>
+                Software Engineering Immersive
+              </h2>
+            </Card.Title>
+            <Card.Subtitle className="mb-3">General Assembly, 2022</Card.Subtitle>
+            <Card.Text className="education-intro-CSS">
+              The Software Engineering Immersive program at General Assembly provides an intensive and hands-on curriculum focusing on full stack web development, where proficiency in the MERN stack is gained to build scalable and dynamic web applications.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Card>
+    </section>
+  );
+}
+
+export default Education;
