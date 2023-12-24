dd58eb194ff1b9a7edae082c87258559db16fda3
rhaelfixer <rhaelfixer@gmail.com>
2023-12-25 08:49:48 +0900

    Update App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+
+import ParticlesEffect from "./views/ParticlesEffect";
+import Navigation from "./views/Navigation";
+import Profile from "./views/Profile";
+import Skills from "./views/Skills";
+import Projects from "./views/Projects";
+import Career from "./views/Career";
+import Education from "./views/Education";
+import Contact from "./views/Contact";
+import Footer from "./views/Footer";
+import Credits from "./views/Credits";
+
+
+import {Scrollbar} from "./components/Scrollbar";
+
+
+// Context
+import {useDate} from "./components/DateContext";
+
+
+// CSS
+import "./App.css";
+
 
 function App() {
+  const {isNewYear, isSummer, isHalloween, isChristmas} = useDate();
+
+  useEffect(() => {
+    Scrollbar({isNewYear, isSummer, isHalloween, isChristmas});
+  }, [isNewYear, isSummer, isHalloween, isChristmas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ParticlesEffect />
+      <Navigation />
+      <Profile />
+      <Skills />
+      <Projects />
+      <Career />
+      <Education />
+      <Contact />
+      <Credits />
+      <Footer />
+    </>
   );
 }
 
