ecc870182cb44e5e4a06779be3c4e0d4df3d5e2c
rhaelfixer <rhaelfixer@gmail.com>
2024-11-03 21:31:26 +0900

    Update Profile

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,11 +1,11 @@
-import React, {useEffect} from "react";
-import {Typewriter} from "react-simple-typewriter";
+import React, { useEffect } from "react";
+import { Typewriter } from "react-simple-typewriter";
 import "animate.css";
 
 
 // Context
-import {useDate} from "../components/DateContext";
-import {useInitialScroll, useScrollOnTop} from "../components/ScrollContext";
+import { useDate } from "../components/DateContext";
+import { useInitialScroll, useScrollOnTop } from "../components/ScrollContext";
 
 
 // CSS
@@ -13,21 +13,20 @@ import "./styles/Profile.css";
 
 
 const Profile = () => {
-  const {isNewYear, isSummer, isHalloween, isChristmas} = useDate();
-  const {scrollOnTop, setScrollOnTop} = useScrollOnTop();
-  const {initialScroll, setInitialScroll} = useInitialScroll();
-
+  const { isNewYear, isSummer, isHalloween, isChristmas } = useDate();
+  const { scrollOnTop, setScrollOnTop } = useScrollOnTop();
+  const { initialScroll, setInitialScroll } = useInitialScroll();
 
   // Define the classes based on your conditions
   let classes = ["profile-name-CSS", "typewriter-CSS", "profile-intro-CSS"];
   if (isNewYear) {
-    classes = classes.map(className => className + " newYear");
+    classes = classes.map((className) => className + " newYear");
   } else if (isSummer) {
-    classes = classes.map(className => className + " summer");
+    classes = classes.map((className) => className + " summer");
   } else if (isHalloween) {
-    classes = classes.map(className => className + " halloween");
+    classes = classes.map((className) => className + " halloween");
   } else if (isChristmas) {
-    classes = classes.map(className => className + " christmas");
+    classes = classes.map((className) => className + " christmas");
   }
 
   // Now "classes" array contains the modified class names
@@ -58,58 +57,79 @@ const Profile = () => {
   return (
     <>
       <section className="profile-CSS">
-        {
-          scrollOnTop
-            ? (
-              <div>
-                <h1 className={`animate__animated animate__fadeIn profile-name-CSS ${ profileNameColor }`}>
-                  Rhael Fixer
-                </h1>
-                <br />
-                <h2 className={`animate__animated animate__fadeIn typewriter-CSS ${ typewriterColor }`}>
-                  <Typewriter
-                    words={["Software", "ReactJS"]}
-                    loop={Infinity}
-                    cursor
-                    cursorStyle="|"
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={3000}
-                  />
-                  {" "}Developer
-                </h2>
-                {initialScroll && (
-                  <h2 className={`animate__animated animate__fadeOut profile-intro-CSS ${ profileIntroColor }`}>
-                    I am a ReactJS developer with strong familiarity with front-end technologies. My expertise lies in designing visually appealing and user-friendly interfaces, ensuring responsive designs work seamlessly across various devices. Additionally, I also have valuable experience in API integration for server-side development. I am constantly looking forward to learn new technologies to build upon my already existing skills.
-                  </h2>
-                )}
-              </div>
-            ) : (
-              <div>
-                <h1 className={`animate__animated animate__fadeOut profile-name-CSS ${ profileNameColor }`}>
-                  Rhael Fixer
-                </h1>
-                <br />
-                <h2 className={`animate__animated animate__fadeOut typewriter-CSS ${ typewriterColor }`}>
-                  <Typewriter
-                    words={["Software", "ReactJS"]}
-                    loop={Infinity}
-                    cursor
-                    cursorStyle="|"
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={3000}
-                  />
-                  {" "}Developer
-                </h2>
-                {!initialScroll && (
-                  <h2 className={`animate__animated animate__fadeIn profile-intro-CSS ${ profileIntroColor }`}>
-                    I am a ReactJS developer with strong familiarity with front-end technologies. My expertise lies in designing visually appealing and user-friendly interfaces, ensuring responsive designs work seamlessly across various devices. Additionally, I also have valuable experience in API integration for server-side development. I am constantly looking forward to learn new technologies to build upon my already existing skills.
-                  </h2>
-                )}
-              </div>
-            )
-        }
+        {scrollOnTop ? (
+          <div>
+            <h1
+              className={`animate__animated animate__fadeIn profile-name-CSS ${profileNameColor}`}
+            >
+              Rhael Fixer
+            </h1>
+            <br />
+            <h2
+              className={`animate__animated animate__fadeIn typewriter-CSS ${typewriterColor}`}
+            >
+              <Typewriter
+                words={["Software", "ReactJS"]}
+                loop={Infinity}
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={3000}
+              />{" "}
+              Developer
+            </h2>
+            {initialScroll && (
+              <h2
+                className={`animate__animated animate__fadeOut profile-intro-CSS ${profileIntroColor}`}
+              >
+                I am a ReactJS developer with strong familiarity with front-end
+                technologies. My expertise lies in designing visually appealing
+                and user-friendly interfaces, ensuring responsive designs work
+                seamlessly across various devices. Additionally, I also have
+                valuable experience in API integration for server-side
+                development. I am constantly looking forward to learn new
+                technologies to build upon my already existing skills.
+              </h2>
+            )}
+          </div>
+        ) : (
+          <div>
+            <h1
+              className={`animate__animated animate__fadeOut profile-name-CSS ${profileNameColor}`}
+            >
+              Rhael Fixer
+            </h1>
+            <br />
+            <h2
+              className={`animate__animated animate__fadeOut typewriter-CSS ${typewriterColor}`}
+            >
+              <Typewriter
+                words={["Software", "ReactJS"]}
+                loop={Infinity}
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={3000}
+              />{" "}
+              Developer
+            </h2>
+            {!initialScroll && (
+              <h2
+                className={`animate__animated animate__fadeIn profile-intro-CSS ${profileIntroColor}`}
+              >
+                I am a ReactJS developer with strong familiarity with front-end
+                technologies. My expertise lies in designing visually appealing
+                and user-friendly interfaces, ensuring responsive designs work
+                seamlessly across various devices. Additionally, I also have
+                valuable experience in API integration for server-side
+                development. I am constantly looking forward to learn new
+                technologies to build upon my already existing skills.
+              </h2>
+            )}
+          </div>
+        )}
       </section>
     </>
   );
