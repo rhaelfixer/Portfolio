9ad4be55d0805ed9b3c1f271d62f35087bb1620d
rhaelfixer <rhaelfixer@gmail.com>
2024-11-03 21:49:41 +0900

    Update Education

--- src/views/Education.js
+++ src/views/Education.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -11,19 +11,20 @@ function Education() {
   return (
     <section className="education-CSS" id="education">
       <Card className="education-outer-card-CSS">
-        <h1 className="education-h1-CSS">
-          Education
-        </h1>
+        <h1 className="education-h1-CSS">Education</h1>
         <Card className="education-inner-card-CSS">
           <Card.Body>
             <Card.Title>
-              <h2>
-                Software Engineering Immersive
-              </h2>
+              <h2>Software Engineering Immersive</h2>
             </Card.Title>
-            <Card.Subtitle className="mb-3">General Assembly, 2022</Card.Subtitle>
+            <Card.Subtitle className="mb-3">
+              General Assembly, 2022
+            </Card.Subtitle>
             <Card.Text className="education-intro-CSS">
-              The Software Engineering Immersive program at General Assembly provides an intensive and hands-on curriculum focusing on full stack web development, where proficiency in the MERN stack is gained to build scalable and dynamic web applications.
+              The Software Engineering Immersive program at General Assembly
+              provides an intensive and hands-on curriculum focusing on full
+              stack web development, where proficiency in the MERN stack is
+              gained to build scalable and dynamic web applications.
             </Card.Text>
           </Card.Body>
         </Card>
